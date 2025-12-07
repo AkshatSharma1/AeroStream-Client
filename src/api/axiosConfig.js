66cefa5ec21b7cb5e28bb666e1b8b5d7f8d8f256
Akshat Ajay <akshat.ajay0931@gmail.com>
2025-12-07 14:16:34 +0530

    Changes for vercel

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -4,10 +4,11 @@ const API_BASE_URL =
   process.env.REACT_APP_API_URL || "http://localhost:3005/api/v1";
 
 const api = axios.create({
-    baseURL: API_BASE_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+    "ngrok-skip-browser-warning": "true",
+  },
 });
 
 // Add a request interceptor to attach the Token
